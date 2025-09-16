892e999e7120eeeaab0ee8c5aaae4b5497bbdb28
Ankit Raj <ankitraj828401@gmail.com>
2025-09-16 12:06:47 +0530

    learned monorepo

---
+++ monorepo-1.0/apps/docs/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button appName="Docs app">
+        Hi there
+      </Button>
+    </div>
+  );
+}
